62c183be75a0eac666a87822d3435d95c4b71cba
sunwei-hh <342451737@qq.com>
2020-05-24 18:06:10 +0800

    000

---
+++ 期末/src/app/login-component/login-component.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+function userNameValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^a/)) {
+    return { invalidUser: true };
+  }
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.css']
+})
+export class LoginComponentComponent implements OnInit {
+
+  myForm: FormGroup;
+
+  userName: AbstractControl;
+
+  password: AbstractControl;
+
+  name$: Observable<string>;
+  authService: any;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group(
+      {
+        'userName': ['aaa', Validators.compose([Validators.required, userNameValidator])],
+        'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+      }
+    );
+
+    this.userName = this.myForm.controls['userName'];
+    this.password = this.myForm.controls['password'];
+    this.name$ = this.userName.valueChanges;
+    this.userName.valueChanges.subscribe(val => {
+      console.log(val);
+    });
+  }
+
+  onSubmit(value: any) {
+    console.log(value);
+  }
+  ngOnInit(): void {
+  }
+  login() {
+    this.authService.login();
+  }
+}
+
